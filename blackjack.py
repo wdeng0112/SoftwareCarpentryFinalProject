4dee9eb1321502d3d13323815c12f19395ea97f9
kengzhang1998 <kengzhang1998@gmail.com>
2023-12-06 21:01:05 -0500

    Update blackjack.py with pygame module

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,18 @@
 # This is the main file for final project
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode([500, 500])
+
+# Variable to determine if the game is running
+running = True
+
+# Main game loop for when the game is running
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((255, 255, 255))
+
+    pygame.display.flip()
+pygame.quit()
